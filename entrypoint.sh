935979e0308828efa4c67ba8bc85fd2f195c4bfe
D.H. Bahr <dhbahr@gmail.com>
2015-09-08 11:11:49 -0400

    Added support for Amazon RDS

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 set -e
 
 # set odoo database host, port, user and password
-: ${PGHOST:=${PGHOST:='localhost'}}
-: ${PGPORT:=${PGPORT:=5432}}
-: ${PGUSER:=${PGUSER:='postgres'}}
-: ${PGPASSWORD:=$PGPASSWORD}
+: ${PGHOST:=${RDS_HOSTNAME:=${PGHOST:='localhost'}}}
+: ${PGPORT:=${RDS_PORT:=${PGPORT:=5432}}}
+: ${PGUSER:=${RDS_USERNAME:=${PGUSER:='postgres'}}}
+: ${PGPASSWORD:=${RDS_PASSWORD:=$PGPASSWORD}}
 export PGHOST PGPORT PGUSER PGPASSWORD
 
 case "$1" in
